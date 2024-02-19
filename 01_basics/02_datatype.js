c3541b5b1a606be66689e68d159cf6b2db8b541d
anurag2775 <159125835+anurag2775@users.noreply.github.com>
2024-02-19 23:27:12 +0530

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,13 @@
+let name = "Anurag "
+let age = 18
+let isLoggedIn = false
+account = 
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "Anurag");
+console.log(typeof account);
+
+//null=stand alone value
+//undefined=abhi tak value assign nahi huyi hai
+//symbol=unique
